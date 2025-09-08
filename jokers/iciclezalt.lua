c92bfb21af1c9b6f129399c468ca74a6274e5ca3
GreenKookie56 <brandon05121@gmail.com>
2025-09-09 05:58:42 +0800

    please work

--- jokers/iciclezalt.lua
+++ jokers/iciclezalt.lua
@@ -57,12 +57,14 @@ G.FUNCS.check_for_buy_space = function(card)
     end
     return check_for_buy_space_ref(card)
 end
-local can_select_card_ref = G.FUNCS.can_select_card
+
+local can_select_card_old = G.FUNCS.can_select_card
 G.FUNCS.can_select_card = function(e)
 	if e.config.ref_table.config.center.key == "j__sholium_iciclezalt" then
-		e.config.colour = G.C.GREEN
-		e.config.button = "use_card"
-	else
-		can_select_card_ref(e)
+        e.config.colour = G.C.GREEN
+        e.config.button = 'use_card'
+    else
+        local ret = can_select_card_old(e)
+        return ret
     end
 end
