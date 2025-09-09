33cfeee60bdcf7cd5ba4eebf24bcd9d75edb59a5
GreenKookie56 <brandon05121@gmail.com>
2025-09-09 19:07:13 +0800

    Update virus.lua

--- jokers/virus.lua
+++ jokers/virus.lua
@@ -27,6 +27,7 @@ SMODS.Joker{ --Virus
     cost = 1,
     rarity = 1,
     blueprint_compat = true,
+    demicoloncompat = true,
     eternal_compat = true,
     perishable_compat = false,
     unlocked = true,
@@ -73,5 +74,27 @@ SMODS.Joker{ --Virus
         end
                 }
         end
+       if context.forcetrigger then
+              SMODS.calculate_effect({func = function()
+            local created_joker = false
+    if #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit then
+        created_joker = true
+        G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    local joker_card = SMODS.add_card({ set = 'Joker', rarity = 'Rare' })
+                    if joker_card then
+                    end
+                    G.GAME.joker_buffer = 0
+                    return true
+                end
+            }))
+            end
+            if created_joker then
+                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_plus_joker'), colour = G.C.BLUE})
+            end
+            return true
+        end}, card)
+          end
     end
 }
