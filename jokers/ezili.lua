15e0d01c5caa87120b92d6df8eba7d1e86cdf2cb
GreenKookie56 <brandon05121@gmail.com>
2025-09-10 20:51:53 +0800

    Add files via upload

--- jokers/ezili.lua
+++ jokers/ezili.lua
@@ -8,7 +8,7 @@ SMODS.Joker{ --Ezili
         ['name'] = 'Ezili',
         ['text'] = {
             [1] = '{C:purple}Balances{} {C:blue}Chips{} and {C:red}Mult{}',
-            [2] = 'on {C:attention}Boss Blind{}'
+            [2] = 'on {C:attention}Big Blind{} and {C:attention}Boss Blind{}'
         },
         ['unlock'] = {
             [1] = ''
@@ -25,7 +25,6 @@ SMODS.Joker{ --Ezili
     cost = 7,
     rarity = 3,
     blueprint_compat = true,
-    demicoloncompat = true,
     eternal_compat = true,
     perishable_compat = true,
     unlocked = true,
@@ -33,19 +32,13 @@ SMODS.Joker{ --Ezili
     atlas = 'CustomJokers',
 
     calculate = function(self, card, context)
-        if context.cardarea == G.jokers and context.joker_main then
-            if G.GAME.blind.boss then
+        if context.cardarea == G.jokers and context.joker_main  then
+            if (G.GAME.blind.boss or G.GAME.blind:get_type() == 'Big') then
                 return {
                     balance = true,
                     message = "Hex"
                 }
             end
-        end
-        if context.forcetrigger then
-            return {
-                balance = true,
-                message = "Hex"
-            }
         end
     end
 }
