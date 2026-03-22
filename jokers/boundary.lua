4921cd06f6f6817009f0b4acceb5ed23a3650c31
GreenKookie56 <brandon05121@gmail.com>
2026-03-22 13:43:38 +0800

    Update boundary.lua

--- jokers/boundary.lua
+++ jokers/boundary.lua
@@ -2,7 +2,6 @@ SMODS.Joker{ --Boundary
     key = "boundary",
     config = {
         extra = {
-            Xmult = 2.718
         }
     },
     loc_txt = {
@@ -44,7 +43,7 @@ SMODS.Joker{ --Boundary
     calculate = function(self, card, context)
         if context.cardarea == G.jokers and context.joker_main or context.forcetrigger then
                 return {
-                    Xmult = card.ability.extra.Xmult
+                    Xmult = 2.718
                 }
         end
     end
